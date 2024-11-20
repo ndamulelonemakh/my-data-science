ab42d45c901df4c9c3f6f4da84494b4f9d6260f8
Ndamulelo Nemakhavhani <nbanzyme@gmail.com>
2024-11-20 20:59:14 +0200

    Create colored_logs.py

---
+++ utils/colored_logs.py
@@ -0,0 +1,7 @@
+import click
+
+i = lambda msg: click.style(msg, fg="blue")
+s = lambda msg: click.style(msg, fg="green")
+e = lambda msg: click.style(msg, fg="red")
+w = lambda msg: click.style(msg, fg="yellow")
+d = lambda msg: click.style(msg, fg="white")
